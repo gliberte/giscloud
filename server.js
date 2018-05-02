ed394af7d505bf0023926738e616757e5da2fc9e
Luis Solano <gliberte@yahoo.com>
2018-05-02 16:43:31 -0500

    fin de la clase 5, seccion 1

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import express from 'express'
 
 const app = express()
 
+app.use('/graphql',(req,res)=>{
+    res.send({data:true})
+})
 app.listen(4000,()=>{
     console.log({estado:"Iniciado"})
 })
